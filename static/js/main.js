a08e09e86b86ab8dd4d5d9ee09778ae28f03ebb4
M Haidar Hanif <git@mhaidarhanif.com>
2017-06-20 06:10:14 +0700

    Update styling

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,1 @@
-console.log("%c Hello World!", "background: hsla(185, 70%, 50%, 1); color: #fff; font-size: 23px;");
+console.log('%c Hello World!', 'background: hsla(185, 70%, 50%, 1); color: #fff; font-size: 23px;')
